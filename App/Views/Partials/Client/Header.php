4eda4dc46357b2c7a573f8b887569f73e3af7fef
Lê Đoan Thy <ledoanthy@gmail.com>
2026-06-03 14:25:03 +0700

    Chỉnh header

--- App/Views/Partials/Client/Header.php
+++ App/Views/Partials/Client/Header.php
@@ -2,6 +2,7 @@
 <html lang="vi">
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/../Web-Application/Public/Client/Css/Client_Global.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
